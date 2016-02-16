b4a612982a5c4b3e7466e871913f8158c082eeb7
Graham Reeves <graham@grahamreeves.com>
2016-02-16 19:09:07 +0000

    Initial demo project, loads file on click

---
+++ Unity/Assets/AbcExample.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+[ExecuteInEditMode]
+public class AbcExample : MonoBehaviour {
+
+	public UnityEngine.UI.Text	mErrorText;
+	public UnityEngine.UI.Text	mFilenameText;
+
+	PopAbc			mAbc;
+
+
+	void OnError(string Error)
+	{
+		Debug.LogError( Error );
+
+		if ( mErrorText == null )
+			return;
+
+		mErrorText.text += Error + "\n";
+	}
+
+	public void LoadAbc()
+	{
+		LoadAbc (mFilenameText.text);
+	}
+
+	public void LoadAbc(string Filename)
+	{
+		mAbc = new PopAbc( Filename );
+	}
+
+	void Update()
+	{
+		PopAbc.FlushDebug (Debug.Log);
+	}
+
+}

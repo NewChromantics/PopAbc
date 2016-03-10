564701335383d401e76fcf7ec49353668c23e44e
Graham Reeves <graham@grahamreeves.com>
2016-03-10 13:22:07 +0000

    Added a basic scene/geo node type to encapsulate a model

--- src/TParser.h
+++ src/TParser.h
@@ -1,7 +1,29 @@
 #pragma once
-
-
 #include <string>
+#include <SoyVector.h>
+#include <HeapArray.hpp>
+
+
+//	very basic geometry classes.
+//	might be time to move this to Soy as we already have vertex descriptions
+
+namespace Geo
+{
+	class TNode;
+}
+
+class Geo::TNode
+{
+public:
+	std::string							mName;
+	float4x4							mTransform;
+	Soy::Bounds3f						mBounds;
+	Array<std::shared_ptr<TNode>>		mChildren;
+	Array<vec3f>						mVertexPositions;
+	Array<uint16>						mTriangleIndexes;
+	std::map<std::string,std::string>	mMeta;			//	unhandled meta data
+};
+
 
 
 class TParserParams
